ed57b6a7f452cb74f12de2be035049d48e454fcb
Oyewole Oni <onioyewole1910@gmail.com>
2022-06-18 09:54:57 +0100

    Data Binding

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  public tourImage:string = "https://images.pexels.com/photos/3184433/pexels-photo-3184433.jpeg?auto=compress&cs=tinysrgb&w=600";
 
-  ngOnInit(): void {
+  public time:string = new Date().toLocaleTimeString();
+  constructor() { 
+    this.updateTine();
   }
 
+  ngOnInit(): void {
+    
+  }
+  public getDate():string{
+    return new Date().toDateString();
+  }
+
+  public getDateTime(): string{
+    return new Date().toLocaleTimeString();
+  }
+
+  public updateTine():void{
+    setInterval(()=> {
+      this.time = new Date().toLocaleTimeString();
+    }, 1000)
+  }
+ 
 }
