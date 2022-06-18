f6103ce8b494d2fc70293d87939f6af7ef9ea3e2
Oyewole Oni <onioyewole1910@gmail.com>
2022-06-18 14:02:12 +0100

    Event Handling Pt 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component'
+import { CardComponent } from './components/card/card.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { WishMessageComponent } from './components/wish-message/wish-message.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CounterComponent,
+    WishMessageComponent
   ],
   imports: [
     BrowserModule,
