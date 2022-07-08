84bcb06f13e1c41ebfba681ba4a9696f0719a016
Oyewole Oni <onioyewole1910@gmail.com>
2022-07-08 17:20:59 +0100

    Angular Routing Dynamic Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
 import { CountryListComponent } from './components/country-list/country-list.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,6 +9,7 @@ import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'contacts', component: ContactsComponent},
+  {path: 'contacts/:id', component: ContactDetailsComponent},
   {path: 'about', component: AboutComponent},
 ];
 
--- src/app/components/contact-details/contact-details.component.ts
+++ src/app/components/contact-details/contact-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Contact } from 'src/app/models/Contact';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contact-details',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailsComponent implements OnInit {
 
-  constructor() { }
+  public contact: Contact = {} as Contact;
+  public contactId : string | null = '';
+  public errorMessage: string | undefined;
+
+  constructor(private activatedRoute: ActivatedRoute, private contactService: ContactService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param: ParamMap) => {
+      this.contactId = param.get('id');
+    })
+
+    this.contactService.getAllContacts().subscribe((data) =>{
+      let conntacts: Contact[] = data;
+      let selectedContact = conntacts.find(x => x.login.uuid === this.contactId);
+
+      if(selectedContact){
+        this.contact = selectedContact;
+      }
+    })
+   
+
   }
 
 }
